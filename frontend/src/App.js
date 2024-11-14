09e866ed6fc64944d1c39bcba9d2858e7e7e826b
jcarpio3 <122296805+jcarpio3@users.noreply.github.com>
2024-11-14 16:44:57 -0700

    Installed Tailwind.css
    Had trouble installing this would just throw errors finally figured it out it was the tailwind.config.js file that needed to be changed
    
    -  Added  "./src/**/*.{js,jsx,ts,tsx}" to content
    was from https://tailwindcss.com/docs/guides/create-react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Test from './components/Test';
-
+import "./index.css"
 const TestProject = { //Testing Props 
     "Title" : "Dynamic Speed Limit Sign",
     "Image" : "https://global.discourse-cdn.com/codecademy/original/5X/3/c/8/5/3c85feecb4eb52a4d69ef0891cfbc495a1da7174.png",
@@ -9,10 +9,10 @@ const TestProject = { //Testing Props
 
 const App = () => {
     return (
-        <>
-            <h1>Test</h1>
+        <div >
+            <h1>Hello World</h1>
             <Test tProp={TestProject}/>
-        </>
+        </div>
     );
 }
 
--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Test = ({tProp}) => {
     return (
         <div className="project">
-            <h1>{tProp.Title}</h1>
+            <h1 className="text-7xl text-center text-blue-400">{tProp.Title}</h1>
             <img src={tProp.Image} alt="img" />
             <br></br>
             <a href={tProp.Github}>Github</a>
